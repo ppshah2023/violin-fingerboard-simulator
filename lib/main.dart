8a041cd27ddd21cd66178f5d89a57652cf0a993c
Pranit Shah <ppshah2023@email.medfield.net>
2021-05-15 12:04:43 -0400

    saving in version history before removing major file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'unused/oldFingerboard-sim.dart';
+import 'fingerboardSim.dart';
 
 void main() async {
   runApp(MyApp());
@@ -13,27 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Fingerboard Simulator'),
+      home: HomePage(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Fingerboard());
   }
 }
